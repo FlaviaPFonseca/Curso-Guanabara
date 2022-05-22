4452a0d5be70cff4b7dbec080095d3caacb573fc
Flávia Poliane Fonseca <104723133+FlaviaPFonseca@users.noreply.github.com>
2022-05-21 23:09:35 -0300

    Entrega final
    Correção da linha que não estava pegando a horado dia' ´$´'; alteração do 'padding' de 'pix' para 'rem'

--- Hora do dia/horadodia.js
+++ Hora do dia/horadodia.js
@@ -1,19 +1,20 @@
 function carregar() {
-    var msg =window.document.getElementById("msg");
-    var img =window.document.getElementById("imagem");
+    var msg =window.document.getElementById('msg');
+    var img =window.document.getElementById('imagem');
     var data = new Date();
     var hora = data.getHours();
-    msg.innerHTML= 'Agora são'${hora}'horas';
-    if(hora>=0 && hora < 12) {
+   msg.innerHTML= `Agora são ${hora} horas.`;
+    if (hora>=0 && hora < 12) {
         //Bom dia!
-        img.src ="fotomanha.png"
+        img.src ='fotomanha.png';
         document.body.style.background ='#e2cd9f';
-      } else if(hora >= 12 && hora >18) {
+      } else if(hora >= 12 && hora <=18) {
         //Boa tarde!
-        img.src="fotodatarde.jpg"
+        img.src= 'fotodatarde.jpg';
         document.body.style.background = '#ab9846f';
     } else (hora >= 12 && hora <18);{
             //Boa noite!
-        img.src="fotonoite.jpg"
-     }document.body.style.background = '#515154';
+        img.src= 'fotonoite.jpg';
+     document.body.style.background = '#515154';
+    }
 }
