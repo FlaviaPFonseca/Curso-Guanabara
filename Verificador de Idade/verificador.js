2f9d7affc920522f1f7a20175d44d83aa3cd3eb6
Flávia Poliane Fonseca <104723133+FlaviaPFonseca@users.noreply.github.com>
2022-05-22 00:53:01 -0300

    Verificador de Idade
    Base, falta fazer ficar responsivo;

---
+++ Verificador de Idade/verificador.js
@@ -0,0 +1,20 @@
+function verificar() {
+var data = new Date()
+var ano = data.getFullYear()
+var fano = document.getElementById('txtano')
+var res = document.querySelector('div#res')
+if (fano.value.length == 0 ||Number (fano.value) > ano){
+window.alert('[ERROR] Veriique os dados e tente novamente!')
+}else {
+    var fsex = document.getElementsByName('radsex')
+    var idade = ano - Number(fano.value)
+  var gênero =''
+  if(fsex[0].checked){
+      gênero = 'Homem'
+  }else if( fsex[1].checked){
+      gênero=  'Mulher'
+  }
+res.style.textAlign ='center'
+res.innerHTML ='Detectamos'${gênero}'com'${idade}'anos';
+}
+}
