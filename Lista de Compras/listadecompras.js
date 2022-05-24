0f71b352334c110ca0106cb5fff3423b5dcef367
Flávia Poliane Fonseca <104723133+FlaviaPFonseca@users.noreply.github.com>
2022-05-23 23:44:01 -0300

    Add files via upload
    arquivo javascript  falta juntar as inteirações dos 3 arquivos

---
+++ Lista de Compras/listadecompras.js
@@ -0,0 +1,10 @@
+function () {
+
+    let text = document.getElementById("texto").value;
+    let list = document.getElementById("lista").innerHTML;
+
+    list +="<li>" + text+ "</li>"
+    document.getElementById("lista").innerHTML= list;
+document.getElementById("texto").value= null
+
+}
