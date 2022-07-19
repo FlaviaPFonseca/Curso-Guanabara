d6ace70b4a6f76b012a9b8864866dd75e8b4243e
Flávia Poliane Fonseca <104723133+FlaviaPFonseca@users.noreply.github.com>
2022-07-18 22:10:03 -0300

    alteração para melhorar

--- Lista de Compras/listadecompras.js
+++ Lista de Compras/listadecompras.js
@@ -1,10 +1,17 @@
-function () {
+  var $lista = document.querySelector('ul');
+    var $produto = document.querySelector('#produto');
+    var $botao = document.querySelector('#btn');
 
-    let text = document.getElementById("texto").value;
-    let list = document.getElementById("lista").innerHTML;
-
-    list +="<li>" + text+ "</li>"
-    document.getElementById("lista").innerHTML= list;
-document.getElementById("texto").value= null
-
-}
+    $botao.addEventListener('click', addProduto);
+   
+function addProduto() {
+      var itens =  '<li>' +$produto.value + '</li>';
+    $lista.innerHTML += itens;
+    $produto.value = '';
+    $produto.focus();
+    }
+    $produto.addEventListener('keyup',function(e){
+     if (e.keyCode === 13){
+      addProduto()
+     } 
+    })
